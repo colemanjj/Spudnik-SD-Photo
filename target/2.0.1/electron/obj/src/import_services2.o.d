23a8c8f93398dc123af5fa5a0521acc72c18dac1
colemanjj <45085414+colemanjj@users.noreply.github.com>
2021-06-20 16:47:09 -0500

    merging 2020 12 01b to the master (#1)
    some early code from particle solar ~30
    
    * Spudnik-SD-Photo_good_22.24
    
    * SD&camera power by 3v3 and grounded through NPN controlled by B2
    
    * 2020-12-13_SD & camera, upload commented out
    
    * 2020-12-17_w_upload commented out
    
    * 2020-12-26_SD-card & camera work, camera on Lipo pin
    
    * 2021-01-02_testing discharge with camera and SD-card
    
    * 2021-01-17_after testing SOC, setting SOC levels for camera and connecting and moving to functions
    
    * 2021-02-21_compiles, tests for usb
    
    * 2021-05-15_commit
    
    * 2021-06-20_w/Cora
    
    * untracking files

---
+++ target/2.0.1/electron/obj/src/import_services2.o.d
@@ -0,0 +1,7 @@
+g:/jsc/IoT/particle/Spudnik-SD-Photo/target/2.0.1/electron//obj/src/import_services2.o: \
+ src/import_services2.c ../../../services/inc/services2_dynalib.h \
+ ../../../dynalib/inc/dynalib.h
+
+../../../services/inc/services2_dynalib.h:
+
+../../../dynalib/inc/dynalib.h:
